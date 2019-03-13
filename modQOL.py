34821bf6a744e0bee50f92c196e81f075ccb9a2c
Matthew <matthewbuckley051@gmail.com>
2019-03-13 21:21:06 +0000

    Added TimeTaken class
    Simple class to calculate time taken for program to run and output it in a readable way.

--- modQOL.py
+++ modQOL.py
@@ -4,8 +4,40 @@ Module containing some Quality of life functions and classes.
 """
 ##### IMPORTS #####
 import os
+import time
 
 ##### CLASSES #####
+class TimeTaken:
+    """
+    Simple class for measuring the time taken for a program to run and outputting it in a readable way.
+    """
+    def __init__(self):
+        """Sets start to currect time when class is instanciated."""
+        self.start = time.clock()
+        return
+        
+    def getTimeTaken(self):
+        """Returns a readable formatted string of the time taken."""
+        #Calc time taken
+        timeTaken = round(time.clock() - self.start)
+        if timeTaken >= 60:
+            timeTakenSecs = int(timeTaken % 60)
+            timeTakenMins = int((timeTaken - timeTakenSecs) / 60)
+            if timeTakenMins >= 60:
+                timeTakenMins = int(timeTakenMins % 60)
+                timeTakenHrs = int((timeTaken - (timeTakenMins * 60) - timeTakenSecs) / 3600)
+                return "{} hrs, {} mins, {} secs.".format(timeTakenHrs, timeTakenMins, timeTakenSecs)
+            else:
+                return "{} mins, {} secs.".format(timeTakenMins, timeTakenSecs)
+        elif timeTaken < 1:
+            return "< 1 sec."
+        else:
+            return "{} secs.".format(timeTaken)
+        
+    def resetStart(self):
+        """Resets the start time, returns new start time."""
+        self.start = time.clock()
+        return self.start
 
 ##### FUNCTIONS #####
 def newDir(dirPath):
@@ -43,8 +75,10 @@ def newDir(dirPath):
         count -= 1
         
     return None
-
+    
 
 ##### TEST CODE #####
 if __name__ == '__main__':
+    timer = TimeTaken()
     print('Testing')
+    print('Time taken', timer.getTimeTaken())
