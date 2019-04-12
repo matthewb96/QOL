8b3338e2365da3da29049327fd1a595d5ca7c237
Matthew <matthewbuckley051@gmail.com>
2019-04-12 14:48:38 +0100

    Created __init__ file so module can be used as package

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    Initialisation module for QOL package.
+"""
+##### MAIN #####
+# List the packages to import when using from pySATURN import *
+__all__ = [
+        'modQOL'
+           ]
