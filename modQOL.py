08bce1b3e54f4be74a70c3f2783a1fb606c0803d
Matthew <matthewbuckley051@gmail.com>
2019-10-04 16:56:11 +0100

    Removed logger class
    Removed the logger class because the logging module provides all the functionality, and much more, that the logger class was attempting to provide.

--- modQOL.py
+++ modQOL.py
@@ -6,8 +6,8 @@ Classes
 ----------
     TimeTaken
         Simple class for measuring the time taken for a program to run and outputting it in a readable way.
-    Logger
-        Class that will allow the user to log to a file while also printing to console
+    ParamerFile
+        Class for handlind parameter files.
 
 Functions
 ----------
@@ -101,111 +101,6 @@ class TimeTaken:
         remainingLap = abs(avgLap - (time.time() - self.laps[-1]))
         timeLeft = avgLap * remainingLaps + remainingLap
         return self._readableTime(timeLeft)
-
-class Logger:
-    """
-        Class that will allow the user to log to a file while also printing to console,
-        inspired by the logging module albeit much simpler.
-
-        Parameters
-        ----------
-        logFile: string
-            Path to the log file.
-        append: boolean, optional
-            Whether or not to append to the logFile (True), if it already exists, or overwrite it (False).
-            Default False.
-        level: string, optional
-            The level of message which to output to console, can be one of 4 values ('DEBUG', 'INFO', 'ERROR', 'FATAL').
-            Default 'INFO'.
-        tags: boolean, optional
-            Whether to include [time][level] on each line of the log file.
-            Default True.
-    """
-    # Class variables
-    lvlVals = {'DEBUG':0, 'INFO':1, 'ERROR':2, 'FATAL':3}
-
-    def __init__(self, logFile, append=False, level='INFO', tags=True):
-        """Initialises the class and sets the console output level and the path to the log file."""
-        self.logFile = logFile
-        self.setLevel(level)
-        self.incTags = tags
-        # Select mode
-        if not append:
-            mode = 'wt'
-        else:
-            mode = 'at'
-        # Create log file (if it doesn't exist already)
-        with open(logFile, mode):
-            pass
-
-        return
-
-    def setLevel(self, level):
-        """
-            Set the console output level to a new value.
-
-            Parameters
-            ----------
-                level: string, optional
-                    The level of message which to output to console, can be one of 4 values ('DEBUG', 'INFO', 'ERROR', 'FATAL').
-                    Default 'INFO'.
-        """
-        # Check level is an acceptable value
-        self.checkLevel(level)
-        self.consoleLvl = level.upper()
-        return
-
-    def checkLevel(self, level):
-        """Checks the level given is allowed, raises ValueError if it isn't."""
-        if level.upper() not in Logger.lvlVals.keys():
-            raise ValueError("level not acceptable value, given '{}' should be {}".format(level, list(Logger.lvlVals.keys())))
-        return
-
-    def getLevel(self):
-        """Returns the current console output level."""
-        return self.consoleLvl
-
-    def getTime(self):
-        """Returns the current time and date as a string"""
-        t = time.localtime()
-        return '{:0>2}/{:0>2}/{:0>2} {:0>2}:{:0>2}:{:0>2}'.format(t[2], t[1], str(t[0])[2:4], t[3], t[4], t[5])
-
-    def printOut(self, *args, level='INFO', newLine=False):
-        """
-            Prints out a message to the console, if level is above console output level, and to the log file.
-
-            Parameters
-            ----------
-                *args: string or multiple strings
-                    String to be printed and written to log file.
-                level: string, optional
-                    The level of importance of the message. Default 'INFO'.
-                newLine: boolean, optional
-                    Whether or not to put each argument on a new line. Default False.
-        """
-        self.checkLevel(level)
-        # Print if level is greater than console level
-        if self.lvlVals[level.upper()] >= self.lvlVals[self.consoleLvl]:
-            if newLine:
-                print(*args, sep='\n')
-            else:
-                print(*args)
-        # Set tags
-        if self.incTags:
-            tags = '[{:17}][{:6}] '.format(self.getTime(), level)
-        else:
-            tags = ''
-        # Get arguments as a string
-        if newLine:
-            output = ''
-            for i in args:
-                output += tags + str(i) + '\n'
-        else:
-            output = tags + ' '.join((str(i) for i in args)) + '\n'
-        # Write to log file
-        with open(self.logFile, 'at') as f:
-            f.write(output)
-        return
 
 class ParameterFile:
     """ Class that can read and write parameter text files, to be used for various variables within a script. """
@@ -361,8 +256,5 @@ def newDir(dirPath, number=False, ignoreEmpty=False):
 ##### TEST CODE #####
 if __name__ == '__main__':
     timer = TimeTaken()
-    log = Logger('test.log', append=True, tags=True)
-    log.printOut('Testing', level='debug')
     test = list(range(10))
-    log.printOut(*test, level='info', newLine=True)
-    log.printOut('Time taken', timer.getTimeTaken())
+    print('Time taken', timer.getTimeTaken())
